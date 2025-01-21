65307a4ba0221dd129ac154c7ffd5e4db5f00036
leah_fort <leah_fort@outlook.com>
2025-01-21 11:49:12 +0000

    1.1.0 Refactor api_actions to make parameters optional at function level.\nAdd repo readme

--- api_actions.py
+++ api_actions.py
@@ -2,28 +2,19 @@
 async def do_request_on(session, request_text, **request_params):
     API_URL = request_params["api_url"]
     API_KEY = request_params["api_key"]
-    MODEL = request_params["model"]
-    TEMPERATURE = request_params["temperature"]
-    TOP_P = request_params["top_p"]
-    MAX_TOKENS = request_params["max_tokens"]
-    FREQUENCY_PENALTY = request_params["frequency_penalty"]
-    PRESENCE_PENALTY = request_params["presence_penalty"]
     
     headers = {
         'Authorization': f'Bearer {API_KEY}',
         'Content-Type': 'application/json'
     }
-    
-    # create request params object
-    params = {
-        "model": MODEL,
-        "temperature": TEMPERATURE,
-        "top_p": TOP_P,
-        "max_tokens": MAX_TOKENS,
-        "frequency_penalty": FREQUENCY_PENALTY,
-        "presence_penalty": PRESENCE_PENALTY
-    }
         
+    MODEL = request_params["model"]
+    params = {"model": MODEL}
+    float_params = ["temperature", "top_p", "frequency_penalty", "presence_penalty"]
+    int_params = ["max_tokens"]
+    params.update({key: float(request_params[key]) for key in float_params if key in request_params})
+    params.update({key: int(request_params[key]) for key in int_params if key in request_params})
+    
     request_body = make_request_body(request_text, **params)
     async with session.post(API_URL, json=request_body, headers=headers) as response:
         if response.status == 200:
